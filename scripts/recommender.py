bb1d532f05606f8920d6c865be1ce4e6a2e50994
Thinh Nguyen <tnntech@gatech.edu>
2018-09-17 11:27:43 -0400

    Recommender add on

--- scripts/recommender.py
+++ scripts/recommender.py
@@ -1,9 +1,7 @@
 import datetime
 import paramiko
+import time
 import sys
-import dataAnalysis
-import logStatement
-import sshConnection
 
 #####################
 #### RECOMMENDER ####
@@ -50,10 +48,14 @@ class Recommender:
 	def getCurrentDateTime(self):
 		return datetime.datetime.now()
 
+
 ##################
 #### ANALYSIS ####
 ##################
 class Analysis(Recommender):
+
+	queues_Data = {}
+	recommended_queue = ''
 
 	# Connect into Georgia Tech PACE Login
 	def sshClientConnect(self):
@@ -65,15 +67,45 @@ class Analysis(Recommender):
 		print("executing qstat ...")
 		print("----------------------------")
 
+	# Generate logs (errors, data, output) to txt file
+	def logsGenerator(self, option):
+		# In case of error (ssh, execute command line)
+			# Write to the txt file
+			# exit the code exit(1)
+
+		# In case no error, able to access data
+			# Save the result to the text file include date and time
+		return
+
+	# Algorithmns to analyze data and compute the best to the worse queue (in order)
+	def recommendedQueue(self, queues_Data):
+		# TODO
+		return
+
+	# Return the best queue to recommend user to run
+	def getRecommendedQueue(self):
+		print("----------------------------")
+		# Get the best queue from queue
+		# recommended_queue = queues_Data[0]
+		return recommended_queue if recommended_queue != '' else 'Please run recommendedQueue function to receive the best queue.'
+
+	# Send the txt file (raw data) including recommended result to user's request
+	def sendResultToUser(self):
+		currentUserEmail = self.username + '@gatech.edu'
+		print ("Email is sending ... Please wait")
+		time.sleep(3)
+		print ("Email has been sent to [ " + self.username + " ]")
+
 
 ################
 #### DRIVER ####
 ################
 if __name__ == '__main__':
+	print ("Welcome To Georgia Tech Recommender System...")
+	time.sleep(1) # Time out for 1 sec
 	username = input("Please enter your GT username: ")
-	password = input("Please enter your password: ")
-	execute = Recommender(username, password)
-	print (execute.getSSHLink())
+	password = input("Please enter your GT password: ")
+	test = Analysis(username, password)
 
 
 
