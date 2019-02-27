6e424b3433359aad181eb433cfc2d011ddfc9c28
Thinh Nguyen <tnntech@gatech.edu>
2019-02-26 22:18:45 -0500

    restructure the code base, get rid of duplicated code, wrote more helper methods to compare time range

--- scripts/dataAnalysis.py
+++ scripts/dataAnalysis.py
@@ -1,5 +1,5 @@
 import datetime
-import recommenderClass as rC
+import recommenderClass as mainClass
 
 # Helper method to check the time has the least
 def compare(time1, time2):
@@ -19,15 +19,12 @@ def compare(time1, time2):
 
 # Estimate the total amount task should split out for each hostname
 def taskSplitRecommender(recommenderQueue, ssh):
-    hostname, nodes, maxData, arrData = '', '', 100.0, []
+    hostname, nodes, maxData, lines = '', '', 100.0, ''
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('pace-check-queue ' + recommenderQueue)
-    filePath = 'HostServerDetail_Data/' + str(getCurrentDateTime())
-    newFile = open(filePath, 'w')
-    newFile.write("Today is: " +  str(getCurrentDateTime()) + '\n')
 
     # Get out the one has the least CPU and core number
     for line in iter(ssh_stdout.readline, ""):
-        newFile.write(line)
+        lines += line
         dataNode = line.split()
         if len(dataNode) >= 8 and dataNode[6] != 'No' and dataNode[2] not in ['Nodes', 'Memory', 'Cpu%']:
             # Calculate the one has the least cpu in use
@@ -35,32 +32,32 @@ def taskSplitRecommender(recommenderQueue, ssh):
                 maxData = float(dataNode[2])
                 hostname = dataNode[0]
                 nodes = dataNode[1]
-    arrData.append(hostname)
-    arrData.append(nodes)
-    arrData.append(str(maxData))
-    newFile.close()
-    return arrData
+
+    # Write new data to logs
+    filePath = 'HostServerDetail_Data/' + str(getCurrentDateTime())
+    writeDataToTxtFile(filePath, lines)
+
+    return [hostname, nodes, str(maxData)]
 
 # Get out the list of queue summary
 def taskNpsByCore(recommenderQueue, ssh):
-    summaryPath = 'hostName_Core_Requested/' + str(getCurrentDateTime()) + '\n'
-    summaryFile = open(summaryPath, 'w')
-    summaryFile.write("Today is: " +  str(getCurrentDateTime()) + '\n')
-    summaryFile.write("" + recommenderQueue + " Queue summary:" + "\n")
-    baseCounter = 2
+    baseCounter, lines = 2, "" + recommenderQueue + " Queue summary:" + "\n"
 
     # Generate the summary logs file base on best recommender queue and base core number
     while baseCounter <= 64:
         ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('pace-check-queue ' + recommenderQueue)
-        summaryFile.write("NUMBER OF TASK/NP REQUESTED: " + '[' + str(baseCounter) + ']' + '\n')
+        lines += "NUMBER OF TASK/NP REQUESTED: " + '[' + str(baseCounter) + ']' + '\n'
         for line in iter(ssh_stdout.readline, ""):
             lineData = line.split()
             if len(lineData) >= 8 and lineData[6] != 'No' and lineData[2] not in ['Nodes', 'Memory', 'Cpu%']:
                 if numberOfCoreLeft(lineData[1]) >= baseCounter:
-                    summaryFile.write(line)
+                    lines += line
         baseCounter *= 2
-        summaryFile.write("\n")
-    summaryFile.close()
+        lines += '\n'
+
+    # Write new data to logs
+    summaryPath = 'hostName_Core_Requested/' + str(getCurrentDateTime())
+    writeDataToTxtFile(summaryPath, lines)
 
 # Return the current date with time
 def getCurrentDateTime():
@@ -69,37 +66,25 @@ def getCurrentDateTime():
 # Collect of the total of walltime of each queues
 def collectWallTimeQueue(ssh, sampleQueues):
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('pace-whoami')
-    walltime = {}
-    filePath = "paceWallTime_Data/" + str(getCurrentDateTime())
-    newFile = open(filePath, 'w')
-    newFile.write("Today is: " +  str(getCurrentDateTime()) + '\n')
+    walltime, lines = {}, ''
+
     for line in iter(ssh_stdout.readline, ""):
+        lines += line
         queue_Node = line.split()
-        newFile.write(line)
         if (len(queue_Node) != 0):
             if queue_Node[0] in sampleQueues:
                 walltime[queue_Node[0]] = queue_Node[1]
-    newFile.close()
+
+    # Write new data to logs
+    filePath = "paceWallTime_Data/" + str(getCurrentDateTime())
+    writeDataToTxtFile(filePath, lines)
+
     return walltime
 
 # Helper method to return the number of core cpu has left in the hostname
 def numberOfCoreLeft(taskNp):
     left, right = taskNp.split('/')
     return int(right) - int(left)
-
-# Return the last execution
-# def lastExecution() -> str:
-#     if justExecuted():
-#         # Read every line in the recently file
-#         # Print out the console
-#     else:
-#         print("ddsds")
-        # Write new data into the recently file
-        # executionPath = "lastExecution/recently"
-        # executionFile = open(executionPath, 'w')
-        # executionFile.write("Today is: " +  str(dA.getCurrentDateTime()) + '\n')
-        # executionFile.write(self.recommended_queue)
-        # executionFile.close()
 
 # Helper method to write data into the txt file
 def writeDataToTxtFile(path: str, data: str):
@@ -115,41 +100,20 @@ def readDataFromTxtFile(path: str):
     lines = openFile.readlines()
     return lines
 
-def compareTime(oldTime: int, newTime: int) -> bool:
-    pass
+def compareTimeRange(oldTime, newTime, time_range) -> bool:
+    # Generate the old time range to real data value
+    oldTimeRange = datetime.datetime(oldTime[0], oldTime[1], oldTime[2], oldTime[3], oldTime[4], oldTime[5])
 
-# Determine if the last executed fall under 10 mins
-def justExecuted(time_range: int) -> bool:
-    # Last execution file
-    line = readDataFromTxtFile("lastExecution/recently")[0]
-
-    # Get the last execution time
-    dateExecuted = line[10:20]
-    timeExecuted = line[21:29]
-
-    oldYear, oldMonth, oldDay = dateExecuted.split("-")
-    oldHour, oldMinutes, oldSecond = timeExecuted.split(":")
-
-    # Get the current time
-    currentTime = str(getCurrentDateTime())
-    date = currentTime[0:10]
-    time = currentTime[11:19]
-
-    year, month, day = date.split("-")
-    hour, minutes, second = time.split(":")
-
-    oldTime = datetime.datetime(int(oldYear), int(oldMonth), int(oldDay), int(oldHour), int(oldMinutes), int(oldSecond))
-
-    if oldTime < datetime.datetime.now():
-        diff_Year = int(year) - int(oldYear)
-        diff_Month = int(month) - int(oldMonth)
-        diff_Date = int(day) - int(oldDay)
+    if oldTimeRange < getCurrentDateTime():
+        diff_Year = newTime[0] - oldTime[0]
+        diff_Month = newTime[1] - oldTime[1]
+        diff_Date = newTime[2] - newTime[2]
 
         if diff_Year or diff_Month or diff_Date:
             return False
 
-        diff_Hour = int(hour) - int(oldHour)
-        diff_Min = int(minutes) - int(oldMinutes)
+        diff_Hour = newTime[3] - oldTime[3]
+        diff_Min =  newTime[4] - oldTime[4]
 
         if diff_Hour > 0:
             return False
@@ -159,3 +123,25 @@ def justExecuted(time_range: int) -> bool:
             else:
                 return False
     return True
+
+# Determine if the last executed fall under 10 mins
+def justExecuted(time_range: int) -> bool:
+    # Last execution file
+    previousExecutedDate = readDataFromTxtFile("lastExecution/recently")[0]
+
+    # Get the last execution time
+    dateExecuted = previousExecutedDate[10:20]
+    timeExecuted = previousExecutedDate[21:29]
+    oldYear, oldMonth, oldDay = dateExecuted.split("-")
+    oldHour, oldMinutes, oldSecond = timeExecuted.split(":")
+    oldTime = [int(oldYear), int(oldMonth), int(oldDay), int(oldHour), int(oldMinutes), int(oldSecond)]
+
+    # Get the current time
+    currentTime = str(getCurrentDateTime())
+    date = currentTime[0:10]
+    time = currentTime[11:19]
+    year, month, day = date.split("-")
+    hour, minutes, second = time.split(":")
+    newTime = [int(year), int(month), int(day), int(hour), int(minutes), int(second)]
+
+    return compareTimeRange(oldTime, newTime, time_range)
