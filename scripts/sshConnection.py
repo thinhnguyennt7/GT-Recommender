bffbddcb5b67a443b582d05574a5832ea6eed19a
Thinh Nguyen <tnntech@gatech.edu>
2019-03-11 13:26:09 -0400

    remove no need files

--- scripts/sshConnection.py
+++
@@ -1,7 +0,0 @@
-import paramiko
-
-# Helper method to connect into the PACE server
-def connectToServer(hostname: str, username: str, password: str):
-	ssh = paramiko.SSHClient()
-	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	ssh.connect(hostname, username=username, password=password)
